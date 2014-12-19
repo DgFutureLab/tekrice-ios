917a3d75f4e67f29d6e980325299929259b26d6a
yutatoga <yuta.toga@gmail.com>
2014-12-19 18:15:56 +0900

    deleted copyright's comment

--- Techrice/main.m
+++ Techrice/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  Techrice
-//
-//  Created by 藤賀 雄太 on 9/18/14.
-//  Copyright (c) 2014 Future Lab. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 #import "AppDelegate.h"
